2b43cb33eb9cf5d3c6c3d32950acb3fca6eceaea
Vishal Jangid <vishaljangid80550786@gmail.com>
2026-02-17 11:57:10 +0530

    commited

--- services/imageService.ts
+++ services/imageService.ts
@@ -48,7 +48,7 @@ export const getAvatarPath = (file: any, isGroup = false) => {
 
   if (file && typeof file == "object") return file.uri;
 
-  if (isGroup) return require("../assets/images/defaultGroupAvatar.png");
+  if (isGroup) return require("../assets/images/defaultgavatar.png");
 
   return require("../assets/images/defaultAvatar.png");
 };
